36c8dc41b07669496ea2cfb1304b22c66a9b1672
Bahey <bahy2002@gmail.com>
2024-05-02 09:19:13 +0300

    .

---
+++ models/billModel.js
@@ -0,0 +1,77 @@
+const mongoose = require("mongoose");
+const jwt = require("jsonwebtoken");
+const { hashPassword } = require("../utils/password");
+
+const Schema = mongoose.Schema;
+
+const billSchema = new mongoose.Schema({
+
+    patientID: {
+        type: String,
+        required: true,
+    },
+    doctorID: {
+        type: String,
+        required: true,
+    },
+    patientName: {
+        type: String,
+        required: true,
+    },
+    doctorName: {
+        type: String,
+        required: true,
+    },
+
+    billAmount: {
+        type: Number,
+        required: true,
+    },
+
+    insuranceCoverage: {
+        type: Number,
+        required: false,
+    },
+    billInsured: {
+        type: String,
+        required: true,
+    },
+    service: {
+        type: String,
+        required: true,
+    },
+    adminName: {
+        type: String,
+        required: true,
+    },
+
+    createdAt: {
+        type: Date,
+        default: Date.now,
+    },
+
+});
+
+/**
+ * Hashes the password before saving the user to the database.
+ * @param {Function} next - Callback function.
+ * @returns {Promise<void>} - Promise that resolves when hashing is done.
+ * @throws {Error} - If there is an error hashing the password or saving the userPreferences.
+ * @async
+ */
+doctorSchema.pre("save", async function (next) {
+    if (!this.isModified("password")) {
+      return next();
+    }
+    try {
+      const hashedPassword = await hashPassword(this.password);
+      this.password = hashedPassword;
+      next();
+    } catch (error) {
+      next(error);
+    }
+  });
+  
+  const Bill = mongoose.model("Bill", billSchema);
+  
+  module.exports = Bill;
