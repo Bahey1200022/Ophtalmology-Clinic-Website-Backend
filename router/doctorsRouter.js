d24cd73f09719849a9671246f803f7fd54be4da8
Bahey <bahy2002@gmail.com>
2024-05-03 22:58:24 +0300

    .

--- router/doctorsRouter.js
+++ router/doctorsRouter.js
@@ -2,8 +2,13 @@ const express = require('express');
 const router = express.Router();
 const doctorAuth = require("../controller/doctors/doctorAuth");
 const doctorController = require("../controller/doctors/doctorController");
+const availabilityController = require("../controller/doctors/availability");
 
 
+
+
+
+router.post("/editAvailability", availabilityController.editAvailability);
 router.post("/doctorLogin", doctorAuth.doctorLogin);
 router.post("/doctorSignUp", doctorAuth.doctorSignUp);
 router.post("/doctorEditProfile", doctorController.editProfile);
